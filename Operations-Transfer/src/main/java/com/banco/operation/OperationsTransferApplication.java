785473f6bf1a2f314afce224aca7b35b244edfcf
jhon <jhonhinostroza1234@gmail.com>
2024-12-31 08:25:41 -0500

    add banners add OperationsTransfer

---
+++ Operations-Transfer/src/main/java/com/banco/operation/OperationsTransferApplication.java
@@ -0,0 +1,13 @@
+package com.banco.operation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationsTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationsTransferApplication.class, args);
+	}
+
+}
